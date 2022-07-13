ab04366e48cbb5ff6c54853da5f8012af0308e71
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-07-13 08:49:21 -0300

    Ajustando o footer da página

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -3,6 +3,8 @@ import './Footer.css';
 
 export default function Footer(props){
     return <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-star text-danger"></i> por <strong>Hyg<span className="text-danger">0</span>r</strong> 
+        </span>
     </footer>
 }
